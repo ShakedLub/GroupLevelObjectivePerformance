eae98ad5f15e93fc9b20569c0587038fa3d7743c
Shaked Lublinsky <77659078+ShakedLub@users.noreply.github.com>
2025-05-29 13:24:28 +0300

    run again dataset relanalysis

--- DatasetsReanalysis/Scripts/Faivre_2014.R
+++ DatasetsReanalysis/Scripts/Faivre_2014.R
@@ -15,7 +15,6 @@ data[data$exp=="Faivre et al. 2","dataset"]="Faivre_et_al_2014_E2"
 data[data$exp=="Faivre et al. 3","dataset"]="Faivre_et_al_2014_E3"
 data[data$exp=="Faivre et al. 4","dataset"]="Faivre_et_al_2014_E4"
 data[data$exp=="Faivre et al. 6","dataset"]="Faivre_et_al_2014_E6"
-data[data$exp=="Faivre et al. 7","dataset"]="Faivre_et_al_2014_E7"
 data[data$exp=="Faivre et al. 8","dataset"]="Faivre_et_al_2014_E8"
 
 data_per_subj[data_per_subj$exp=="Faivre et al. 1","dataset"]="Faivre_et_al_2014_E1"
@@ -23,7 +22,6 @@ data_per_subj[data_per_subj$exp=="Faivre et al. 2","dataset"]="Faivre_et_al_2014
 data_per_subj[data_per_subj$exp=="Faivre et al. 3","dataset"]="Faivre_et_al_2014_E3"
 data_per_subj[data_per_subj$exp=="Faivre et al. 4","dataset"]="Faivre_et_al_2014_E4"
 data_per_subj[data_per_subj$exp=="Faivre et al. 6","dataset"]="Faivre_et_al_2014_E6"
-data_per_subj[data_per_subj$exp=="Faivre et al. 7","dataset"]="Faivre_et_al_2014_E7"
 data_per_subj[data_per_subj$exp=="Faivre et al. 8","dataset"]="Faivre_et_al_2014_E8"
 
 data_per_subj=subset(data_per_subj,select=-exp)
@@ -32,10 +30,6 @@ data=subset(data,select=-exp)
 #create paper variable
 data$paper="F_2014"
 data_per_subj$paper="F_2014"
-
-#exclude exp 7 as the objective aware subejcts' data is not given
-data_per_subj=filter(data_per_subj,dataset != "Faivre_et_al_2014_E7")
-data=filter(data,dataset != "Faivre_et_al_2014_E7")
 
 #add column excObjTest
 data_per_subj$excObjTest=ifelse(data_per_subj$SR>0.65,1,0)
