610cf1c7682e6299901b31ed400d1287c3039722
Itai Yaron <itayyaron@tau.ac.il>
2026-06-10 18:58:31 +0300

    Removed JPG outputs for simulations + got fresh PNGs for option 1 and option 4 (3 figures, one main 2 SUP)

--- Simulations/Scripts/GrandScatterPlot.R
+++ Simulations/Scripts/GrandScatterPlot.R
@@ -11,7 +11,7 @@ pkgs <- c("tidyverse","wesanderson", "stringr", "dplyr", "gridExtra", "svglite",
 groundhog.library(pkgs, "2025-03-01", tolerate.R.version = '4.5.1')
 
 ## parameters
-optionImage = 5
+optionImage = 1
 #1 = test types: GBC, T, MMLR, GBBayes, TBayes
 #create also a Bayesian image:  GBBayes, TBayes, with H0 for supplementary
 #2 = test types: GBC, GB, Chi
